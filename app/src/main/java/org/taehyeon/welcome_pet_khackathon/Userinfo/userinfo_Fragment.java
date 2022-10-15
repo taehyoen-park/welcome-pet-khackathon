ad334f837dc3f0fa3c6be32e29fae02fb70119e0
HyoEun Cho <chohe3212@naver.com>
2022-10-15 22:26:13 +0900

    공인인증 다이얼로그 띄우기 완료

--- app/src/main/java/org/taehyeon/welcome_pet_khackathon/Userinfo/userinfo_Fragment.java
+++ app/src/main/java/org/taehyeon/welcome_pet_khackathon/Userinfo/userinfo_Fragment.java
@@ -1,9 +1,11 @@
 package org.taehyeon.welcome_pet_khackathon.Userinfo;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -62,6 +64,33 @@ public class userinfo_Fragment extends Fragment {
                 Intent intent = new Intent(getActivity(), Login.class);
                 startActivity(intent);
                 getActivity().finish();
+            }
+        });
+
+        btn_certi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dig = new AlertDialog.Builder(getContext());
+
+                dig.setTitle("공인인증하기");
+                dig.setMessage("공인인증을 하시겠습니까?\n(공인인증되는 직종 : 동물훈련전문가, 동물병원종사자, 반려동물관련 자격증 소유자).");
+                dig.setIcon(R.drawable.ic_launcher_foreground).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(),"현재는 인증할 수 없습니다.",Toast.LENGTH_SHORT).show();
+
+                        //displayToast("ok");
+                    }
+                });//onClickListner : ok누르면 뭘할지,
+                dig.setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(),"취소 되었습니다.",Toast.LENGTH_SHORT).show();
+
+                        //displayToast("no");
+                    }
+                });//no버튼 누르면 뭐할지 _토스트를 하게 했다.//두가지 버전으로 가능.~.~; .~;
+                dig.show();
             }
         });
 
